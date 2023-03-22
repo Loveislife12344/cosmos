daea2e0e2cf8833ff97b8398a21a35a86f8d7245
Akash <ak.web.creater@gmail.com>
2023-03-22 12:19:59 +0530

    new js added

--- assets/custom.js
+++ assets/custom.js
@@ -1,7 +1,18 @@
 window.addEventListener("DOMContentLoaded", (event) => {
 // multisteps form
+var stapper = docuemnt.querySelector('.login-step-toggler');
+stapper.addEventListener('click',function(){
+    var allSteps = document.querySelectorAll('.login-tab .step');
+    var activeStep = document.querySelector('.login-tab .step.show');
+    var nextStep = activeStep.nextElementSibling;
+    nextStep.classList.add('show')
+    activeStep.classList.remove('show')
+    if(nextStep.classList.contains('last--step')){
+        stapper.classList.add('d-none')
+        document.querySelector('.login-submit').classList.remove('d-none')
+    }
 
-
+})
 
 
   });
