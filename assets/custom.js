42981cf3ffd58fcd4faee1f70c8df8467323fc22
Akash <ak.web.creater@gmail.com>
2023-03-23 17:40:11 +0530

    new js added

--- assets/custom.js
+++ assets/custom.js
@@ -1,6 +1,7 @@
 window.addEventListener("DOMContentLoaded", (event) => {
 // multisteps form
 var stapper = document.querySelector('.login-step-toggler');
+var Registerstapper = document.querySelector('.register-step-toggler');
 stapper.addEventListener('click',function(){
     var allSteps = document.querySelectorAll('.login-tab .step');
     var activeStep = document.querySelector('.login-tab .step.show');
@@ -10,6 +11,18 @@ stapper.addEventListener('click',function(){
     if(nextStep.classList.contains('last--step')){
         stapper.classList.add('d-none')
         document.querySelector('.login-submit').classList.remove('d-none')
+    }
+
+})
+Registerstapper.addEventListener('click',function(){
+    var allSteps = document.querySelectorAll('.register-tab .step');
+    var activeStep = document.querySelector('.register-tab .step.show');
+    var nextStep = activeStep.nextElementSibling;
+    nextStep.classList.add('show')
+    activeStep.classList.remove('show')
+    if(nextStep.classList.contains('last--step')){
+        stapper.classList.add('d-none')
+        document.querySelector('.register-submit').classList.remove('d-none')
     }
 
 })
