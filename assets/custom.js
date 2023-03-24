967bbeafd3d7e6b8ecda09fb4d02fe98c90215e6
Akash <ak.web.creater@gmail.com>
2023-03-24 12:43:58 +0530

    new js added

--- assets/custom.js
+++ assets/custom.js
@@ -19,19 +19,10 @@ window.addEventListener("DOMContentLoaded", (event) => {
     function BackStep(activeStep,SubmitBtn,nextStepToggler,backStepToggler) {
     
         var backStep = activeStep.previousElementSibling;
-        console.log( "under backstep back " + backStep)
-        console.log( backStep)
-        console.log( "under backstep active" + activeStep)
-        console.log( activeStep)
-        console.log( backStep.classList)
-        console.log( activeStep.classList)
         backStep.classList.add('show')
         activeStep.classList.remove('show')
         SubmitBtn.classList.add('d-none')
-        console.log(nextStepToggler)
-        nextStepToggler.classList.add('show')
         nextStepToggler.classList.remove('d-none')
-        console.log(nextStepToggler.classList)
         if (backStep.classList.contains('step--1')) {
             backStepToggler.classList.add('d-none')
         }
