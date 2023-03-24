a3d79d129c06da3b9695e740ca3f0c63550b9047
Akash <ak.web.creater@gmail.com>
2023-03-24 11:32:24 +0530

    new js added

--- assets/custom.js
+++ assets/custom.js
@@ -1,39 +1,59 @@
 window.addEventListener("DOMContentLoaded", (event) => {
-// multisteps form
-var stapper = document.querySelector('.login-step-toggler');
-var Registerstapper = document.querySelector('.register-step-toggler');
-if(stapper != null){
-    stapper.addEventListener('click',function(){
-        var allSteps = document.querySelectorAll('#signin .step');
-        var activeStep = document.querySelector('#signin .step.show');
+    // multisteps form
+
+    // next step function
+    function NextStep(activeStep, nextStepToggler, backStepToggler, submitBtn) {
         var nextStep = activeStep.nextElementSibling;
         nextStep.classList.add('show')
         activeStep.classList.remove('show')
-        if(nextStep.classList.contains('final--step')){
-            stapper.classList.add('d-none')
-            document.querySelector('.login-submit').classList.remove('d-none')
+        if (activeStep.classList.contains('step--1')) {
+            backStepToggler.classList.remove('d-none')
         }
-    
-    })
-}
-if(Registerstapper != null){
-    Registerstapper.addEventListener('click',function(){
-        var allSteps = document.querySelectorAll('.register-tab .step');
-        var activeStep = document.querySelector('.register-tab .step.show');
-        var nextStep = activeStep.nextElementSibling;
-        nextStep.classList.add('show')
-    
+        if (nextStep.classList.contains('final--step')) {
+            nextStepToggler.classList.add('d-none')
+            submitBtn.classList.remove('d-none')
+        }
+    }
+
+    // Back Step funtion
+    function BackStep(activeStep) {
+        var backStep = activeStep.previousElementSibling;
+        backStep.classList.add('show')
         activeStep.classList.remove('show')
-     
-        if(nextStep.classList.contains('last--step')){
-           
-            Registerstapper.classList.add('d-none')
-            document.querySelector('.register-submit').classList.remove('d-none')
+        if (backStep.classList.contains('step--1')) {
+            backStep.classList.add('d-none')
         }
-    
-    })
-}
+    }
+// variables 
+    var stapper = document.querySelector('.login-step-toggler');
+    var loginActiveStep = document.querySelector('#signin .step.show');
+    var loginBackStep = document.querySelector('.login-beck-step-toggler');
+    var loginSubmitBtn = document.querySelector('.login-submit');
+    var Registerstapper = document.querySelector('.register-step-toggler');
+    var registerActiveStep = document.querySelector('#register .step.show');
+    var registerBackStep = document.querySelector('.register-beck-step-toggler');
+    var ragisterSubmitBtn = document.querySelector('.register-submit');
+// events ------
 
+// login
+    if (stapper != null) {
+        stapper.addEventListener('click', function () {
+            NextStep(loginActiveStep, stapper, loginBackStep, loginSubmitBtn)
+        })
+    }
+    if (loginBackStep != null) {
+        loginBackStep.addEventListener('click', function () {
+            BackStep(loginActiveStep)
+        })
+    }
 
-  });
-  
+    // ragister 
+    if (Registerstapper != null) {
+        Registerstapper.addEventListener('click', function () {
+            NextStep(registerActiveStep, Registerstapper, registerBackStep, ragisterSubmitBtn)
+        })
+    }
+    if (registerBackStep != null) {
+        BackStep(registerActiveStep)
+    }
+});
