78ed6c83eea16407a25ea8b870bb8d5883dadea6
Soniya <soniya@spicybroccoli.com>
2023-05-10 14:41:50 +0530

    return page  changes

--- assets/custom.js
+++ assets/custom.js
@@ -142,7 +142,7 @@ function doSomething(scrollPos) {
         // asd
         document.querySelectorAll('.mn-lv1.mn-has-child').forEach(submenu => {
             var pos = window.getComputedStyle(submenu, ':before').getPropertyValue('top');
-            var newPos = pos - parseInt(scrollPos);
+            var newPos = pos - scrollPos;
             console.log("newPos", newPos)
         })
 
